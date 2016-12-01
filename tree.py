6850f57ea0910db71cb01788faf68a04b6c90dd5
James Carney <jmcarneyiii@gmail.com>
2016-11-30 23:41:54 -0500

    Implement lookup and rename stuff

--- tree.py
+++ tree.py
@@ -4,11 +4,21 @@ class BinarySearchTree:
 
     def insert(self, value):
         if not self.root:
-            self.root = TreeNode(value)
+            self.root = BinarySearchTreeNode(value)
         else:
             self.root.insert(value)
+        return self.root
 
-class TreeNode:
+    def lookup(self, value):
+        if not self.root:
+            return None
+        elif self.root.value == value:
+            return self.root
+        else:
+            return self.root.lookup(value)
+
+
+class BinarySearchTreeNode:
     def __init__(self, value):
         self.value = value
         self.left = None
@@ -17,11 +27,23 @@ class TreeNode:
     def insert(self, value):
         if value < self.value:
             if not self.left:
-                self.left = TreeNode(value)
+                self.left = BinarySearchTreeNode(value)
+                return self.left
             else:
                 self.left.insert(value)
         elif value > self.value:
             if not self.right:
-                self.right = TreeNode(value)
+                self.right = BinarySearchTreeNode(value)
+                return self.right
             else:
                 self.right.insert(value)
+        return None
+
+    def lookup(self, value):
+        if self.value == value:
+            return self
+        elif value < self.value and self.left:
+            return self.left.lookup(value)
+        elif value > self.value and self.right:
+            return self.right.lookup(value)
+        return None
